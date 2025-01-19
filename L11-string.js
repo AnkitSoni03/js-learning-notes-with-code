1c9c4e6ba425996de427e93c51028ce03ddce152
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-01-19 23:25:43 +0530

    Add new files

---
+++ L11-string.js
@@ -0,0 +1,6 @@
+const name = "Ankit";
+const repoCount = 50;
+
+console.log( repoCount + name + " value"); // return 50Ankit Value
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);// prefered Method and return Hello my name is Ankit and my repo count is 50
