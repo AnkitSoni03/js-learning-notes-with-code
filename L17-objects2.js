7812df8ae07497b0c284c3fce5fc044d9999d69c
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-01-24 13:05:51 +0530

    update

--- L17-objects2.js
+++ L17-objects2.js
@@ -77,3 +77,32 @@ console.log(Object.entries(user1)); // return [ [ '1', 'a' ], [ '2', 'b' ], [ '3
 console.log(user1.hasOwnProperty(3)); // return true
 console.log(user1.hasOwnProperty(4)); // return false
 
+/*
+
+JavaScript Objects Summary
+Creating Objects:
+
+Singleton Object: Created using the new Object() syntax, but it is rarely used.
+Object Literal: A simpler and more common way to create objects using curly braces {}.
+Adding Properties:
+Properties can be added dynamically to an object using dot notation or square bracket notation.
+
+Nested Objects:
+Objects can contain other objects as properties. These are referred to as nested objects, and their values can be accessed using dot notation for each level.
+
+Merging Objects:
+
+Objects can be merged using the Object.assign() method or the spread operator (...).
+The spread operator is preferred for its simplicity and readability.
+Objects in Arrays:
+Arrays can hold multiple objects as elements. Each object can have its own set of key-value pairs. Specific properties can be accessed using the array index and the object's key.
+
+Key Object Methods:
+
+Object.keys(): Returns an array of all the keys in the object.
+Object.values(): Returns an array of all the values in the object.
+Object.entries(): Returns an array of key-value pairs in the object.
+hasOwnProperty(): Checks whether a specific key exists in the object.
+These concepts are essential for working with objects in JavaScript efficiently.
+
+*/
