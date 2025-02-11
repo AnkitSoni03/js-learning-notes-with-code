5b78a65f338c104c729dbb853dce475a537f9853
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-02-11 14:55:55 +0530

    add

--- classes-and-oops/getter-setter.js
+++ classes-and-oops/getter-setter.js
@@ -1,21 +1,21 @@
 class User {
-    constructor(email, password){
+    constructor(email, password) {
         this.email = email;
         this.password = password
     }
 
-    get email(){
+    get email() {
         return this._email.toUpperCase()
     }
-    set email(value){
+    set email(value) {
         this._email = value
     }
 
-    get password(){
+    get password() {
         return `${this._password}hitesh`
     }
 
-    set password(value){
+    set password(value) {
         this._password = value
     }
 }
