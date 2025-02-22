21a849236a5bfbc9351c56a96074c08071ecc747
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-02-22 12:23:24 +0530

    updates

--- L1-L30/L5-data-types.js
+++ L1-L30/L5-data-types.js
@@ -1,4 +1,4 @@
-"use strint"; // treat all js code as newer version
+"use strict"; // treat all js code as newer version
 
 //alert("Ankit")  // we are using nodejs, not browser 
 
@@ -10,13 +10,17 @@ let state = null // ignore undefined value
 
 /* Datatypes in JS (premitive)
  
-1. number => 2  to power 53
+1. number => 2  to power 53 [ range ->  (-2^53 -1)  to (2^53 -1) ]
 2. string => " "
 3. boolean => true or false
 4. undefined
 5. null => empty value
-6. bigint
+6. bigint => number which is greater than 2^53 or no limit
 7. symbol => unique
+
+Summary
+✅ Primitive: String, Number, BigInt, Boolean, Undefined, Null, Symbol
+✅ Non-Primitive: Object (including Arrays & Functions)
 
 */
 
